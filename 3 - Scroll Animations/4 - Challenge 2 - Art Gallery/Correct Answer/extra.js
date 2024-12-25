ed52858ac601991b6e7a5b66193bfb195bbbe6e3
yushanwebdev <yushanwebdev@gmail.com>
2024-12-25 20:04:17 +0530

    feat: 3.4 Challenge 2 - Art Gallery

---
+++ 3 - Scroll Animations/4 - Challenge 2 - Art Gallery/Correct Answer/extra.js
@@ -0,0 +1,36 @@
+function randomType(element, characters, duration, sequential = false) {
+  let originalText = element.innerText;
+  let textArray = originalText.split("");
+  let charactersArray = characters.split("");
+  let startTime = new Date().getTime();
+  let interval;
+
+  if (sequential) {
+    let currentIndex = 0;
+    interval = setInterval(function () {
+      textArray[currentIndex] =
+        charactersArray[Math.floor(Math.random() * charactersArray.length)];
+      element.innerText = textArray.join("");
+      currentIndex++;
+      if (currentIndex === textArray.length) {
+        currentIndex = 0;
+      }
+      if (new Date().getTime() - startTime >= duration) {
+        clearInterval(interval);
+        element.innerText = originalText;
+      }
+    }, 20);
+  } else {
+    interval = setInterval(function () {
+      for (let i = 0; i < textArray.length; i++) {
+        textArray[i] =
+          charactersArray[Math.floor(Math.random() * charactersArray.length)];
+      }
+      element.innerText = textArray.join("");
+      if (new Date().getTime() - startTime >= duration) {
+        clearInterval(interval);
+        element.innerText = originalText;
+      }
+    }, 0);
+  }
+}
